b63c3d108d58500d90b840144c1f92fef714820e
Jim Vercoelen <jimvercoelen@Jims.local>
2017-11-29 14:02:08 +0200

    Included Radar and Bar graph components (are showing with dummy data) Included header drop down Fine tuned layout styling

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,14 +3,42 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import styles from './Home.scss'
 import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  MenuItem,
   FormGroup,
   ControlLabel,
   FormControl
 } from 'react-bootstrap'
+import RadarGraph from 'component/RadarGraph'
+import BarGraph from 'component/BarGraph'
+
+import * as MenuOptions from 'constants/MenuOptions'
 
 const initialState = {
-  // Empty
+  // graphOption: MenuOptions.MULTI_GRAPHS
+  graphOption: MenuOptions.POLAR_GRAPH
 }
+
+const radarData = [
+  { subject: 'Math', A: 120, B: 110, fullMark: 150 },
+  { subject: 'Chinese', A: 98, B: 130, fullMark: 150 },
+  { subject: 'English', A: 86, B: 130, fullMark: 150 },
+  { subject: 'Geography', A: 99, B: 100, fullMark: 150 },
+  { subject: 'Physics', A: 85, B: 90, fullMark: 150 },
+  { subject: 'History', A: 65, B: 85, fullMark: 150 }
+]
+
+const barData = [
+  {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+  {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+  {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+  {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+  {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+  {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+  {name: 'Page G', uv: 3490, pv: 4300, amt: 2100}
+]
 
 export class Home extends Component {
   static propTypes = {
@@ -19,6 +47,36 @@ export class Home extends Component {
 
   static defaultProps = {
     // Empty
+  }
+
+  handleMenuGraphOptionClick = (graphOption) => () => {
+    this.setState({ graphOption })
+  }
+
+  handlePrintClick = () => {
+    console.log('print')
+  }
+
+  handleDownloadClick = () => {
+    console.log('Download')
+  }
+
+  get header () {
+    return (
+      <Navbar className={styles.header}>
+        <Nav pullRight>
+          <NavDropdown title='Menu'>
+            <MenuItem onClick={this.handleMenuGraphOptionClick(MenuOptions.MULTI_GRAPHS)}>Multi graphs</MenuItem>
+            <MenuItem onClick={this.handleMenuGraphOptionClick(MenuOptions.POLAR_GRAPH)}>Polar graphs</MenuItem>
+            <MenuItem onClick={this.handleMenuGraphOptionClick(MenuOptions.BAR_GRAPH)}>Bar graphs</MenuItem>
+            <MenuItem onClick={this.handleMenuGraphOptionClick(MenuOptions.TABLE)}>Table</MenuItem>
+            <MenuItem divider />
+            <MenuItem onClick={this.handlePrintClick}>Print</MenuItem>
+            <MenuItem onClick={this.handleDownloadClick}>Download</MenuItem>
+          </NavDropdown>
+        </Nav>
+      </Navbar>
+    )
   }
 
   get sidebar () {
@@ -32,7 +90,6 @@ export class Home extends Component {
             <option value='option3'>option 3</option>
           </FormControl>
         </FormGroup>
-
         <FormGroup>
           <ControlLabel>Region</ControlLabel>
           <FormControl componentClass='select'>
@@ -41,7 +98,6 @@ export class Home extends Component {
             <option value='option3'>option 3</option>
           </FormControl>
         </FormGroup>
-
         <FormGroup>
           <ControlLabel>Scenario collection</ControlLabel>
           <FormControl componentClass='select'>
@@ -50,7 +106,6 @@ export class Home extends Component {
             <option value='option3'>option 3</option>
           </FormControl>
         </FormGroup>
-
         <FormGroup>
           <ControlLabel>Scenario</ControlLabel>
           <FormControl componentClass='select'>
@@ -59,7 +114,6 @@ export class Home extends Component {
             <option value='option3'>option 3</option>
           </FormControl>
         </FormGroup>
-
         <FormGroup>
           <ControlLabel>Scenario</ControlLabel>
           <FormControl componentClass='select' multiple>
@@ -68,7 +122,6 @@ export class Home extends Component {
             <option value='option3'>option 3</option>
           </FormControl>
         </FormGroup>
-
         <FormGroup>
           <ControlLabel>Indicators</ControlLabel>
           <FormControl componentClass='select' multiple>
@@ -77,7 +130,6 @@ export class Home extends Component {
             <option value='option3'>option 3</option>
           </FormControl>
         </FormGroup>
-
         <FormGroup>
           <ControlLabel>Period</ControlLabel>
           <FormControl componentClass='select'>
@@ -86,7 +138,6 @@ export class Home extends Component {
             <option value='option3'>option 3</option>
           </FormControl>
         </FormGroup>
-
         <FormGroup>
           <ControlLabel>Graph type</ControlLabel>
           <FormControl componentClass='select'>
@@ -99,9 +150,26 @@ export class Home extends Component {
     )
   }
 
-  get content () {
+  get innerContent () {
+    let graphElement = null
+    switch (this.state.graphOption) {
+      case MenuOptions.POLAR_GRAPH: {
+        graphElement = <RadarGraph data={radarData} />
+        break
+      }
+      case MenuOptions.BAR_GRAPH: {
+        graphElement = <BarGraph data={barData} />
+        break
+      }
+      default: {
+        graphElement = null
+      }
+    }
+
     return (
-      <div className={styles.content} />
+      <div className={styles.innerContent}>
+        {graphElement}
+      </div>
     )
   }
 
@@ -112,18 +180,21 @@ export class Home extends Component {
   }
 
   componentWillMount () {
-
+    // Empty
   }
 
   componentWillReceiveProps (nextProps) {
-
+    // Empty
   }
 
   render () {
     return (
       <div className={styles.component}>
-        {this.sidebar}
-        {this.content}
+        {this.header}
+        <div className={styles.content}>
+          {this.sidebar}
+          {this.innerContent}
+        </div>
       </div>
     )
   }
