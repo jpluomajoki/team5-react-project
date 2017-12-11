76558d1c0f9fca876ebabc88afe1b9ff8e1ff46b
Bart van Keersop <bart.van.keersop@gmail.com>
2017-12-11 17:23:33 +0200

    Added HELP header item
    HELP Guide available for the user in the header component.
    
    Works in the same way as al the other Modals.

--- src/constants/InformationHTML.js
+++ src/constants/InformationHTML.js
@@ -5,13 +5,14 @@ export const SCENARIOS_INDICATOR = "SCENARIOS_INFORMATION";
 export const INDICATORS_INDICATOR = "INDICATORS_INFORMATION";
 export const TIMEPERIOD_INDICATOR = "TIMEPERIOD_INFORMATION";
 export const DEFAULT_INDICATOR = "DEFAULT_INFORMATION";
+export const GENERAL_INFORMATION_INDICATOR = "GENERAL_INFORMATION";
 
 export const CLOSE_INDICATOR = "CLOSE";
 
 export const INFORMATION_EN = {
   [REGIONLEVEL_INDICATOR]: {
     "title": "regionlevel title ENGLISH",
-    "content": "<h1>Sed haec omittamus;</h1> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Huius, Lyco, oratione locuples, rebus ipsis ielunior. Sed haec nihil sane ad rem; Quis enim redargueret? Duo Reges: constructio interrete. <i>Odium autem et invidiam facile vitabis.</i> Quis istud possit, inquit, negare? Omnia peccata paria dicitis. </p> <ul> <li>Quia nec honesto quic quam honestius nec turpi turpius.</li> <li>Concinnus deinde et elegans huius, Aristo, sed ea, quae desideratur, a magno philosopho, gravitas, in eo non fuit;</li> <li>Quamquam non negatis nos intellegere quid sit voluptas, sed quid ille dicat.</li> <li>Et nunc quidem quod eam tuetur, ut de vite potissimum loquar, est id extrinsecus;</li> <li>Sed virtutem ipsam inchoavit, nihil amplius.</li> <li>Beatus sibi videtur esse moriens.</li> </ul> <h2>Id est enim, de quo quaerimus.</h2> <p>Nemo igitur esse beatus potest. Non est igitur voluptas bonum. Equidem e Cn. <b>Ergo, inquit, tibi Q.</b> Iam in altera philosophiae parte. Quod totum contra est. </p> <p>Ego vero isti, inquam, permitto. Quae sequuntur igitur? Sed haec omittamus; </p> <p><i>Certe, nisi voluptatem tanti aestimaretis.</i> Hic ambiguo ludimur. Immo alio genere; Istam voluptatem, inquit, Epicurus ignorat? Hunc vos beatum; </p> <p>Haeret in salebra. Murenam te accusante defenderem. Aliter homines, aliter philosophos loqui putas oportere? Sed haec nihil sane ad rem; Urgent tamen et nihil remittunt. <i>ALIO MODO.</i> </p>"
+    "content": "regionlevel content ENGLISH"
   },
   [REGION_INDICATOR]: {
     "title": "region title ENGLISH",
@@ -32,6 +33,10 @@ export const INFORMATION_EN = {
   [TIMEPERIOD_INDICATOR]: {
     "title": "timeperiod title ENGLISH",
     "content": "timeperiod content ENGLISH"
+  },
+  [GENERAL_INFORMATION_INDICATOR]: {
+    "title": "General Information (English)",
+    "content": "<h1>Sed haec omittamus;</h1> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Huius, Lyco, oratione locuples, rebus ipsis ielunior. Sed haec nihil sane ad rem; Quis enim redargueret? Duo Reges: constructio interrete. <i>Odium autem et invidiam facile vitabis.</i> Quis istud possit, inquit, negare? Omnia peccata paria dicitis. </p> <ul> <li>Quia nec honesto quic quam honestius nec turpi turpius.</li> <li>Concinnus deinde et elegans huius, Aristo, sed ea, quae desideratur, a magno philosopho, gravitas, in eo non fuit;</li> <li>Quamquam non negatis nos intellegere quid sit voluptas, sed quid ille dicat.</li> <li>Et nunc quidem quod eam tuetur, ut de vite potissimum loquar, est id extrinsecus;</li> <li>Sed virtutem ipsam inchoavit, nihil amplius.</li> <li>Beatus sibi videtur esse moriens.</li> </ul> <h2>Id est enim, de quo quaerimus.</h2> <p>Nemo igitur esse beatus potest. Non est igitur voluptas bonum. Equidem e Cn. <b>Ergo, inquit, tibi Q.</b> Iam in altera philosophiae parte. Quod totum contra est. </p> <p>Ego vero isti, inquam, permitto. Quae sequuntur igitur? Sed haec omittamus; </p> <p><i>Certe, nisi voluptatem tanti aestimaretis.</i> Hic ambiguo ludimur. Immo alio genere; Istam voluptatem, inquit, Epicurus ignorat? Hunc vos beatum; </p> <p>Haeret in salebra. Murenam te accusante defenderem. Aliter homines, aliter philosophos loqui putas oportere? Sed haec nihil sane ad rem; Urgent tamen et nihil remittunt. <i>ALIO MODO.</i> </p>"
   }
 };
 
@@ -59,6 +64,10 @@ export const INFORMATION_FI = {
   [TIMEPERIOD_INDICATOR]: {
     "title": "timeperiod title FINNISH",
     "content": "timeperiod content FINNISH"
+  },
+  [GENERAL_INFORMATION_INDICATOR]: {
+    "title": "General Information (Finnish)",
+    "content": "<h1>Sed haec omittamus;</h1> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Huius, Lyco, oratione locuples, rebus ipsis ielunior. Sed haec nihil sane ad rem; Quis enim redargueret? Duo Reges: constructio interrete. <i>Odium autem et invidiam facile vitabis.</i> Quis istud possit, inquit, negare? Omnia peccata paria dicitis. </p> <ul> <li>Quia nec honesto quic quam honestius nec turpi turpius.</li> <li>Concinnus deinde et elegans huius, Aristo, sed ea, quae desideratur, a magno philosopho, gravitas, in eo non fuit;</li> <li>Quamquam non negatis nos intellegere quid sit voluptas, sed quid ille dicat.</li> <li>Et nunc quidem quod eam tuetur, ut de vite potissimum loquar, est id extrinsecus;</li> <li>Sed virtutem ipsam inchoavit, nihil amplius.</li> <li>Beatus sibi videtur esse moriens.</li> </ul> <h2>Id est enim, de quo quaerimus.</h2> <p>Nemo igitur esse beatus potest. Non est igitur voluptas bonum. Equidem e Cn. <b>Ergo, inquit, tibi Q.</b> Iam in altera philosophiae parte. Quod totum contra est. </p> <p>Ego vero isti, inquam, permitto. Quae sequuntur igitur? Sed haec omittamus; </p> <p><i>Certe, nisi voluptatem tanti aestimaretis.</i> Hic ambiguo ludimur. Immo alio genere; Istam voluptatem, inquit, Epicurus ignorat? Hunc vos beatum; </p> <p>Haeret in salebra. Murenam te accusante defenderem. Aliter homines, aliter philosophos loqui putas oportere? Sed haec nihil sane ad rem; Urgent tamen et nihil remittunt. <i>ALIO MODO.</i> </p>"
   }
 };
 
