d4fd2d842f64ad0c5f8dd57b737255559dd33c19
Bart van Keersop <bart.van.keersop@gmail.com>
2017-12-11 17:07:23 +0200

    Added multiple languages and HTML markup option for modals
    uses dangerously set HTML!

--- src/constants/InformationHTML.js
+++ src/constants/InformationHTML.js
@@ -6,34 +6,64 @@ export const INDICATORS_INDICATOR = "INDICATORS_INFORMATION";
 export const TIMEPERIOD_INDICATOR = "TIMEPERIOD_INFORMATION";
 export const DEFAULT_INDICATOR = "DEFAULT_INFORMATION";
 
-export const INFORMATION = {
+export const CLOSE_INDICATOR = "CLOSE";
+
+export const INFORMATION_EN = {
   [REGIONLEVEL_INDICATOR]: {
-    "title": "regionlevel title",
-    "content": "regionlevel content"
+    "title": "regionlevel title ENGLISH",
+    "content": "<h1>Sed haec omittamus;</h1> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Huius, Lyco, oratione locuples, rebus ipsis ielunior. Sed haec nihil sane ad rem; Quis enim redargueret? Duo Reges: constructio interrete. <i>Odium autem et invidiam facile vitabis.</i> Quis istud possit, inquit, negare? Omnia peccata paria dicitis. </p> <ul> <li>Quia nec honesto quic quam honestius nec turpi turpius.</li> <li>Concinnus deinde et elegans huius, Aristo, sed ea, quae desideratur, a magno philosopho, gravitas, in eo non fuit;</li> <li>Quamquam non negatis nos intellegere quid sit voluptas, sed quid ille dicat.</li> <li>Et nunc quidem quod eam tuetur, ut de vite potissimum loquar, est id extrinsecus;</li> <li>Sed virtutem ipsam inchoavit, nihil amplius.</li> <li>Beatus sibi videtur esse moriens.</li> </ul> <h2>Id est enim, de quo quaerimus.</h2> <p>Nemo igitur esse beatus potest. Non est igitur voluptas bonum. Equidem e Cn. <b>Ergo, inquit, tibi Q.</b> Iam in altera philosophiae parte. Quod totum contra est. </p> <p>Ego vero isti, inquam, permitto. Quae sequuntur igitur? Sed haec omittamus; </p> <p><i>Certe, nisi voluptatem tanti aestimaretis.</i> Hic ambiguo ludimur. Immo alio genere; Istam voluptatem, inquit, Epicurus ignorat? Hunc vos beatum; </p> <p>Haeret in salebra. Murenam te accusante defenderem. Aliter homines, aliter philosophos loqui putas oportere? Sed haec nihil sane ad rem; Urgent tamen et nihil remittunt. <i>ALIO MODO.</i> </p>"
   },
   [REGION_INDICATOR]: {
-    "title": "region title",
-    "content": "region content"
+    "title": "region title ENGLISH",
+    "content": "region content ENGLISH"
   },
   [SCENARIOCOLLECTION_INDICATOR]: {
-    "title": "scenariocollection title",
-    "content": "scenariocollection content"
+    "title": "scenariocollection title ENGLISH",
+    "content": "scenariocollection content ENGLISH"
   },
   [SCENARIOS_INDICATOR]: {
-    "title": "scenarios title",
-    "content": "scenarios content"
+    "title": "scenarios title ENGLISH",
+    "content": "scenarios content ENGLISH"
   },
   [INDICATORS_INDICATOR]: {
-    "title": "indicators title",
-    "content": "indicators content"
+    "title": "indicators title ENGLISH",
+    "content": "indicators content ENGLISH"
   },
   [TIMEPERIOD_INDICATOR]: {
-    "title": "timeperiod title",
-    "content": "timeperiod content"
-  },
-  [DEFAULT_INDICATOR]: {
-    "title": "Oops... something went wrong!",
-    "content":
-      "Data is not available yet, or something went wrong while loading it. Please contact the service provider."
+    "title": "timeperiod title ENGLISH",
+    "content": "timeperiod content ENGLISH"
   }
+};
+
+export const INFORMATION_FI = {
+  [REGIONLEVEL_INDICATOR]: {
+    "title": "regionlevel title FINNISH",
+    "content": "regionlevel content FINNISH"
+  },
+  [REGION_INDICATOR]: {
+    "title": "region title FINNISH",
+    "content": "region content FINNISH"
+  },
+  [SCENARIOCOLLECTION_INDICATOR]: {
+    "title": "scenariocollection title FINNISH",
+    "content": "scenariocollection content FINNISH"
+  },
+  [SCENARIOS_INDICATOR]: {
+    "title": "scenarios title FINNISH",
+    "content": "scenarios content FINNISH"
+  },
+  [INDICATORS_INDICATOR]: {
+    "title": "indicators title FINNISH",
+    "content": "indicators content FINNISH"
+  },
+  [TIMEPERIOD_INDICATOR]: {
+    "title": "timeperiod title FINNISH",
+    "content": "timeperiod content FINNISH"
+  }
+};
+
+export const DEFAULT_INFORMATION = {
+  "title": "Oops... something went wrong!",
+  "content":
+    "Data is not available yet, or something went wrong while loading it. Please contact the service provider."
 };
